da538ad67056bcb1090ef21bb78af6c3275c329c
ViaX: Trace <yankeyank90@gmail.com>
2026-07-17 12:50:31 -0300

    fix: type templatesStreamProvider as List<Template> instead of dynamic

--- lib/features/templates/providers/templates_providers.dart
+++ lib/features/templates/providers/templates_providers.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../../core/database/app_database.dart';
+import '../../../core/database/daos/templates_dao.dart';
 import '../data/template_repository.dart';
 
 final templateRepositoryProvider = Provider<TemplateRepository>((ref) {
@@ -8,7 +9,7 @@ final templateRepositoryProvider = Provider<TemplateRepository>((ref) {
   return TemplateRepository(db);
 });
 
-final templatesStreamProvider = StreamProvider.family<dynamic, String?>((ref, channel) {
+final templatesStreamProvider = StreamProvider.family<List<Template>, String?>((ref, channel) {
   final repo = ref.watch(templateRepositoryProvider);
   return repo.watchAll(channel: channel);
 });
