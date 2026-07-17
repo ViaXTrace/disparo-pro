4d1308e2c44658c4d2a13cc6a81c31085b1aa10e
ViaX: Trace <yankeyank90@gmail.com>
2026-07-17 13:11:04 -0300

    fix: templates_screen.dart

--- lib/features/templates/presentation/templates_screen.dart
+++ lib/features/templates/presentation/templates_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
-import '../../../core/database/daos/templates_dao.dart';
+import '../../../core/database/app_database.dart';
 import '../../templates/providers/templates_providers.dart';
 
 class TemplatesScreen extends ConsumerWidget {
